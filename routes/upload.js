2fc1fa37118221945697af4d5aa91b14e8d244b9
Jorge Rea <jorge.lrs497@gmail.com>
2020-07-13 21:52:48 -0500

    deploy 16

--- routes/upload.js
+++ routes/upload.js
@@ -7,8 +7,19 @@ app.use(fileUpload())
 
 app.post('/', async (req, res) => {
     if (req.files) {
+        let exts = ['png', 'jpg', 'jpeg', 'gif']
         let file = req.files.archivo
-        file.mv('uploads/img.jpg', (err) => {
+        let extension = file.name.split('.')
+        let ext = extension[extension.length - 1]
+
+        if(exts.indexOf(ext) < 0){
+            return res.status(500).json({
+                status: 'error',
+                error: 'extension no valida'
+            })
+        }
+
+        file.mv(`uploads/img.${ext}`, (err) => {
             if (err) {
                 return res.status(500).json({
                     status: 'error',
@@ -16,7 +27,8 @@ app.post('/', async (req, res) => {
                 })
             }
             return res.status(200).json({ 
-                status: 'OK', 
+                status: 'OK',
+                file: `${Date.now()}.${ext}`, 
                 msg: 'imagen subida correctamente' 
             })
         })
