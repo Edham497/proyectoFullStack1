71e14f1ea5dac2aa1572a16bde65003d10799efb
Jorge Rea <jorge.lrs497@gmail.com>
2020-07-13 21:28:14 -0500

    deploy 12

---
+++ routes/upload.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import fileUpload from 'fileUpload'
+
+const app = express()
+
+app.use(fileUpload())
+
+app.post('/', async (req, res) => {
+    if (req.files) {
+        let file = req.files.archivo
+        file.mv('/uploads/img.jpg', (err) => {
+            if (err) {
+                return res.status(500).json({
+                    status: 'error',
+                    error: err
+                })
+            }
+            return res.status(200).json({ 
+                status: 'OK', 
+                msg: 'imagen subida correctamente' 
+            })
+        })
+    }
+})
+
+export default app
